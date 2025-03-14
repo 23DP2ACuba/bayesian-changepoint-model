cf2a7a6a87c9b2d5275d96268d955e922d2e4482
unknown <A230926AC@rvt.lv>
2025-03-14 22:05:58 +0200

    model

--- bayesian_change_point_detector.py
+++ bayesian_change_point_detector.py
@@ -10,22 +10,25 @@ import pandas_ta as ta
 import pandas as pd
 
 # -------- config -------
-SYMBOL = "ETH-USD"
+tfdist = tfp.distributions
+tfbij = tfp.bijectors
+SYMBOL = "TSLA"
 PERIOD = "1d"
-START = "2023-01-01"
+INTERVAL = "1h"
+START = "2024-01-01"
 END = "2025-01-01"
-WINDOW = 20
+WINDOW = 50
 plt.style.use("ggplot")
-LAMBDA_RATE = 5.0
-NUM_SAMPLES = 1000
-NUM_BURNIN = 500
+LAMBDA_RATE = 10.0
+NUM_SAMPLES = 2000
+NUM_BURNIN = 1000
 NUM_CHAINS = 2
-MAX_NUM_CHANGEPOINTS = 1000
+MAX_NUM_CHANGEPOINTS = 100
 
 # -----------------------
 
 # gather data
-data = yfinance.Ticker(SYMBOL).history(period=PERIOD, start=START, end=END)
+data = yfinance.Ticker(SYMBOL).history(period=PERIOD, interval = INTERVAL, start=START, end=END)
 
 # feature creation
 def compute_std_dev(prices: pd.Series, log_returns: bool = True, window: int = 20, min_periods: int = 1) -> pd.Series:
@@ -38,10 +41,10 @@ def OCV_pct(open: pd.Series, close: pd.Series, volume: pd.Series, window: int = 
     ocv_pct = _oc * (volume / _avg_vol)
     return ocv_pct
 
-data['SMA_50'] = np.log(ta.sma(data['Close'], length=50))
-data['SMA_200'] = np.log(ta.sma(data['Close'], length=200))
-data['EMA_50'] = np.log(ta.ema(data['Close'], length=50))
-data['EMA_200'] = np.log(ta.ema(data['Close'], length=200))
+data['SMA_10'] = np.log(ta.sma(data['Close'], length=50))
+data['SMA_20'] = np.log(ta.sma(data['Close'], length=200))
+data['EMA_10'] = np.log(ta.ema(data['Close'], length=50))
+data['EMA_20'] = np.log(ta.ema(data['Close'], length=200))
 data["StdDev"] = compute_std_dev(data.Close, window=WINDOW)
 data["HL_pct"] = (data.High - data.Low) / data.Open
 data["OCV_pct"] = OCV_pct(close=data.Close, open=data.Open, volume=data.Volume, window=WINDOW)
@@ -50,24 +53,44 @@ features = ["Close", "StdDev", "HL_pct", "OCV_pct"]
 data = data[features]
 data.fillna(0, inplace=True)
 
-def log_likelihood(data, changepoints, LAMBDA_RATE):
-  poisson_dist = tfp.distributions.Poisson(rate=LAMBDA_RATE)
-  log_prob = tf.reduce_sum(poisson_dist.log_prob(data))
-  return log_prob
+# Model definition
+class BayesianChangePointModel:
+    def __init__(self, data, lambda_rate=1.0, max_num_changepoints=MAX_NUM_CHANGEPOINTS):
+        self.data = tf.convert_to_tensor(data['Close'].values, dtype=tf.float32)
+        self.num_points = len(data)
+        self.poisson_prior = tfdist.Poisson(rate=lambda_rate)
+        self.max_num_changepoints = max_num_changepoints
+        
+    def joint_log_prob(self, changepoint_locs, means, stddevs):
+        num_changepoints = tf.cast(tf.shape(changepoint_locs)[0], tf.float32)
+        prior_cp = self.poisson_prior.log_prob(num_changepoints)
+        
+        rv_cp_locs = tfdist.Uniform(0, tf.cast(self.num_points, tf.float32))
+        rv_means = tfdist.Normal(loc=0., scale=20.)
+        rv_stddevs = tfdist.HalfNormal(scale=0.5)
+        
+        # Convert changepoint locations to integers and add boundaries
+        cp_locs_int = tf.cast(changepoint_locs, tf.int32)
+        all_cps = tf.concat([tf.constant([0], dtype=tf.int32), 
+                           cp_locs_int, 
+                           tf.constant([self.num_points], dtype=tf.int32)], axis=0)
+        
+        # Compute segment likelihoods using tf.map_fn
+        def compute_segment_likelihood(i):
+            return
+        
+        segment_indices = tf.range(tf.size(all_cps) - 1)
+        log_likelihood = tf.reduce_sum(
+            tf.map_fn(compute_segment_likelihood, segment_indices, dtype=tf.float32)
+        )
+        
+        return (
+            prior_cp +
+            tf.reduce_sum(rv_cp_locs.log_prob(changepoint_locs)) +
+            tf.reduce_sum(rv_means.log_prob(means)) +
+            tf.reduce_sum(rv_stddevs.log_prob(stddevs)) +
+            log_likelihood
+        )
 
-n = 100
-log_likelihoods = []
-for cp in range(1, n):
-    log_likelihoods.append(log_likelihood(data, cp, LAMBDA_RATE))
-
-plt.plot(range(1, n), log_likelihoods)
-plt.axvline(x=120, color='r', linestyle='--', label="True Changepoint")
-plt.title("Log Likelihood for Different Changepoint Locations")
-plt.xlabel("Changepoint Location")
-plt.ylabel("Log Likelihood")
-plt.legend()
-plt.show()
-
-
-most_probable_changepoint = np.argmax(log_likelihoods) + 1
-print(f"Most probable changepoint location: {most_probable_changepoint}")
+# Initialize model
+model = BayesianChangePointModel(data)
